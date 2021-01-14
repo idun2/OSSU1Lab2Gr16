e57c7bb4905dabac7da8ecb19bba3730f1c178c6
Rasmus Olsson <60217980+S192312@users.noreply.github.com>
2021-01-14 10:14:22 +0100

    V7

---
+++ StartGUI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hogskolan_Sarob
+{
+    public partial class StartGUI : Form
+    {
+        public StartGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void läggTillTaBortLärareToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            HogskolanGUI objfrmHome = HogskolanGUI.GetChildInstance();
+            objfrmHome.MdiParent = this;
+            objfrmHome.WindowState = FormWindowState.Maximized;
+            objfrmHome.Show();
+            objfrmHome.BringToFront();
+        }
+    }
+}
